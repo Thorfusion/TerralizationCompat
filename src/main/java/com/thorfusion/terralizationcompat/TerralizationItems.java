6f4118a5802e8f6b49894da7f50e920c072a08eb
maggi373 <40539743+maggi373@users.noreply.github.com>
2022-05-22 00:23:55 +0200

    test stuff

---
+++ src/main/java/com/thorfusion/terralizationcompat/TerralizationItems.java
@@ -0,0 +1,22 @@
+package com.thorfusion.terralizationcompat;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+
+public class TerralizationItems {
+    /*
+    public static Item quartzdust;
+
+    public static void init() {
+        quartzdust = new Item().setUnlocalizedName("quartzdust").setTextureName(TerralizationCompat.MODID + ":quartzdust").setCreativeTab(TerralizationCompat.tabTerralization);
+    }
+
+    public static void register() {
+        GameRegistry.registerItem(quartzdust, quartzdust.getUnlocalizedName());
+        OreDictionary.registerOre("dustQuartz", new ItemStack(quartzdust, 1, 0));
+        OreDictionary.registerOre("dustNetherQuartz", new ItemStack(quartzdust, 1, 0));
+    }
+    */
+}
